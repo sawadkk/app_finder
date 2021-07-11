6c30d07a42501a33f02b15e82d35395730443f3b
sawad kk <60354165+sawadkk@users.noreply.github.com>
2021-07-11 12:10:54 +0530

    Update settings.py

--- finder/settings.py
+++ finder/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gpl)x9=67m=wv21)dpmi0fqyun-sexc0cu$#-=&i)(izagf&lx'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
